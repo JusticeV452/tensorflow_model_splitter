231a717ca6d10b85e37d99678440a94d8ef8d248
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2024-08-30 09:55:30 -0400

    Updated splitters to take list of layers as input

--- model_splitter.py
+++ model_splitter.py
@@ -408,13 +408,14 @@ def split_by_num_segments(num_segments: int, independent_activations=False):
 
     """
 
-    def splitter(model: keras.Model):
-        all_model_layers = list(iter_layers(model))
+    def splitter(layers: list | tuple | keras.Model):
+        if isinstance(layers, keras.Model):
+            layers = list(iter_layers(layers))
         grouped_layers = group_layers(
-            all_model_layers,
+            layers,
             independent_activations=independent_activations
         )
-        layers_per_segment = len(all_model_layers) / num_segments
+        layers_per_segment = len(layers) / num_segments
         smallest_group = min(grouped_layers, key=len)
         assert layers_per_segment >= len(smallest_group), (
             "Not enough layers for {num_segments} segements when "
@@ -459,37 +460,43 @@ def split_by_size(target_max_size: int | float):
     def contains_input_layer(layers):
         return len(list(filter(is_input_layer, layers))) > 0
 
-    def splitter(model: keras.Model):
+    def splitter(layers: list | tuple | keras.Model):
         segment_lengths = []
         current_segment_layers = []
-        all_layers = list(iter_layers(model))
 
-        for i, layer in enumerate(all_layers):
+        if isinstance(layers, keras.Model):
+            layers = list(iter_layers(layers))
+
+        for i, layer in enumerate(layers):
             current_segment_layers.append(layer)
             if len(current_segment_layers) == 1 and is_input_layer(layer):
                 continue
             segment = model_wrap(current_segment_layers)
             segment_size = calc_model_size(segment, units="KB")
 
-            if segment_size >= target_max_size or i == len(all_layers) - 1:
-                next_segment_layers = []
-                if (
-                    segment_size > target_max_size
-                    and len(current_segment_layers) > 1
-                    and not (
-                        len(current_segment_layers) == 2
-                        and contains_input_layer(current_segment_layers)
-                    )
-                ):
-                    # Move last layer in segment to next segment if segment too large
-                    last_added_layer = current_segment_layers.pop(-1)
-                    next_segment_layers.append(last_added_layer)
+            # Continue extending segment if there are more segments
+            # and current segment is smaller than requested
+            if segment_size < target_max_size and i != len(layers) - 1:
+                continue
 
-                segment_lengths.append(len(current_segment_layers))
-                # Put remaing layer in segment of its own
-                if i == len(all_layers) - 1 and next_segment_layers:
-                    segment_lengths.append(1)
-                current_segment_layers = next_segment_layers
+            next_segment_layers = []
+            if (
+                segment_size > target_max_size
+                and len(current_segment_layers) > 1
+                and not (
+                    len(current_segment_layers) == 2
+                    and contains_input_layer(current_segment_layers)
+                )
+            ):
+                # Move last layer in segment to next segment if segment too large
+                last_added_layer = current_segment_layers.pop(-1)
+                next_segment_layers.append(last_added_layer)
+
+            segment_lengths.append(len(current_segment_layers))
+            # Put remaing layer in segment of its own
+            if i == len(layers) - 1 and next_segment_layers:
+                segment_lengths.append(1)
+            current_segment_layers = next_segment_layers
 
         return segment_lengths
 
