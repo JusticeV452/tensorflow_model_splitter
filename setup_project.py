ea45c04ab720817ba5e4c79eaddb4a06958b3122
Justice Vidal <jmvidal@mit.edu>
2024-09-06 16:19:58 -0400

    Fixed crash if nnom directory exists

--- setup_project.py
+++ setup_project.py
@@ -1,11 +1,15 @@
+import os
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.run([
-        "git", "clone",
-        "https://github.com/majianjia/nnom.git",
-        "--branch", "v0.4.3", "nnom"
-    ], check=True)
+    if os.path.exists("nnom"):
+        print("nnom dir exists, skipping repo clone.")
+    else:
+        subprocess.run([
+            "git", "clone",
+            "https://github.com/majianjia/nnom.git",
+            "--branch", "v0.4.3", "nnom"
+        ], check=True)
     subprocess.run([
         "pip", "install", "-r",
         "requirements.txt"
